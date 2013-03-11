146f9cd23fa5af78c7607fee91df7fde1e664876
misko <misko@cs.toronto.edu>
2013-03-11 17:54:50 -0400

    Multithraeded, memory efficient

--- clustering/cluster.cpp
+++ clustering/cluster.cpp
@@ -8,6 +8,7 @@
 #include <string>
 #include <map>
 #include <set>
+#include <omp.h>
 
 using namespace std;
 #define MIN(a,b) (((a)<(b))?(a):(b))
@@ -21,9 +22,13 @@ using namespace std;
 #define WEIRD_STDDEV	4
 #define MAX_STDDEV	15
 
+#define THREADS	6
+
+#define READ_SIZE	1000000
 
 class pos {
 	public:
+		bool marked;
 		int chr;
 		unsigned int coord;
 		bool strand; // true is positive
@@ -51,13 +56,13 @@ class cluster {
 double mean=0.0;
 double stddev=0.0;
 
-map< string , pos > mappings;
 map< pos, map < pos, cluster > > clusters; 
 
 pos::pos(int chr, unsigned int coord, bool strand) {
 	this->chr=chr;
 	this->coord=coord;
 	this->strand=strand;
+	this->marked=false;
 }
 
 
@@ -74,6 +79,7 @@ pos::pos() {
 	chr=-1;
 	coord=-1;
 	strand=false;
+	marked=false;
 }
 
 bool pos::operator<(const pos &other) const {
@@ -179,7 +185,7 @@ unsigned int cigar_len(const char * s) {
 }
 
 
-pair<pos, pos> find_cluster(pos & a, pos & b) {
+void update_cluster(pos & a, pos & b) {
 
 	if (a>b) {
 		pos t = a;
@@ -212,7 +218,14 @@ pair<pos, pos> find_cluster(pos & a, pos & b) {
 						if (xp.chr==b.chr && xp.strand==b.strand) {
 							if ((b.coord+stddev*MAX_STDDEV > xp.coord) || (xp.coord+stddev*MAX_STDDEV > b.coord)) {
 								//found a match!
-								return pair<pos,pos>(p,xp);
+								//return pair<pos,pos>(p,xp);
+								if (a.marked) {
+									(xleft_it->second).lefts.insert(a);
+								}
+								if (b.marked) {
+									(xleft_it->second).rights.insert(b);
+								}
+								return;
 							}
 						}	
 						xleft_it++;
@@ -224,10 +237,17 @@ pair<pos, pos> find_cluster(pos & a, pos & b) {
 	}
 	
 	//make a new entry
-	clusters[a][b]=cluster(a.strand,b.strand);
+	cluster c  =cluster(a.strand,b.strand);
+	if (a.marked) {
+		c.lefts.insert(a);
+	}
+	if (b.marked) {
+		c.rights.insert(b);
+	}
+	clusters[a][b]=c;
 	
-	return pair<pos,pos>(a,b);	
-
+	//return pair<pos,pos>(a,b);	
+	return ;
 
 }
 
@@ -257,13 +277,82 @@ int to_chr(const char * s) {
 
 
 
+#define P	7919 
+
+unsigned int hash_string(const char * s) {
+	unsigned int z=0;
+	for (int i=0; i<strlen(s); i++) {
+		z+=s[i]*P+z*(P+1);
+	}
+	//cerr << "HASH " << s << " -> " << z << endl;
+	return z;
+}
+
+
+void process_read(vector<string> & v_row) {
+	//ok now we have a line lets check it out
+	if (v_row[0].c_str()[0]=='@') {
+		//its header
+		//cerr << row << endl;
+	} else {
+		int flags = atoi(v_row[1].c_str());
+		//check if both are mapped
+		if ((flags & (UNMAPPED + M_UNMAPPED))==0) {
+			string qname = v_row[0];
+			int my_chr = to_chr(v_row[2].c_str());
+			unsigned long my_pos = atol(v_row[3].c_str());
+			bool my_strand = ((flags & REVERSE)==0);
+
+			if (my_strand) {
+				string my_cigar = v_row[5];
+				my_pos+=cigar_len(my_cigar.c_str());
+			}
+
+			int mate_chr = my_chr;
+			if (v_row[6].c_str()[0]!='=') {
+				mate_chr = to_chr(v_row[6].c_str());	
+			}
+			unsigned long mate_pos = atol(v_row[7].c_str());
+			bool mate_strand = ((flags & M_REVERSE)==0);
+		
+			double isize=1000*mean; //TODO: HARD THRESHOLD
+			if (mate_chr==my_chr) {
+				isize=atof(v_row[8].c_str());
+				if (isize<0) {
+					isize=-isize;
+				}
+			}
+
+			if (isize<(WEIRD_STDDEV*stddev+mean)) {
+				//this is kinda normal
+				return;
+			}
+
+
+			//cerr << my_chr << ":" << my_pos << " " << mate_chr << ":" << mate_pos << endl;
+
+			pos my =  pos(my_chr,my_pos,my_strand);
+			my.marked=true;
+			pos mate = pos(mate_chr,mate_pos,mate_strand);
+
+
+			#pragma omp critical 
+			{
+				update_cluster(my,mate);
+			}	
+			
+			
+		}
+	}
+	return ;
+}
 
 int main(int argc, char ** argv) {
 	if (argc<3) {
 		cerr << argv[0]  << " mean stddev" << endl;
 		exit(1);
 	}
-
+	omp_set_num_threads(THREADS);
 	mean=atof(argv[1]);
 	stddev=atof(argv[2]);
 
@@ -274,98 +363,66 @@ int main(int argc, char ** argv) {
 
 	cerr << "Using mean " << mean << " and stddev " << stddev << endl;
 
-	unsigned int read = 0;
 
 	//got from online to read tab input
 	char const row_delim = '\n';
 	char const field_delim = '\t';
-	for (string row; getline(cin, row, row_delim); ) {
-		read++;
-		if (read%1000000==0) {
-			cerr << "\r" << "read: " << read << "     "; 
-		}
-		vector<string> v_row;
-		istringstream ss(row);
-		for (string field; getline(ss, field, field_delim); ) {
-			v_row.push_back(field);
-		}
-		//ok now we have a line lets check it out
-		if (v_row[0].c_str()[0]=='@') {
-			//its header
-			cerr << row << endl;
-		} else {
-			int flags = atoi(v_row[1].c_str());
-			//check if both are mapped
-			if ((flags & (UNMAPPED + M_UNMAPPED))==0) {
-				string qname = v_row[0];
-				int my_chr = to_chr(v_row[2].c_str());
-				unsigned long my_pos = atol(v_row[3].c_str());
-				bool my_strand = ((flags & REVERSE)==0);
 
-				if (my_strand) {
-					string my_cigar = v_row[5];
-					my_pos+=cigar_len(my_cigar.c_str());
-				}
 
-				int mate_chr = my_chr;
-				if (v_row[6].c_str()[0]!='=') {
-					mate_chr = to_chr(v_row[6].c_str());	
-				}
-				unsigned long mate_pos = atol(v_row[7].c_str());
-				bool mate_strand = ((flags & M_REVERSE)==0);
-			
-				double isize=1000*mean; //TODO: HARD THRESHOLD
-				if (mate_chr==my_chr) {
-					isize=atof(v_row[8].c_str());
-					if (isize<0) {
-						isize=-isize;
-					}
-				}
-
-				if (isize<(WEIRD_STDDEV*stddev+mean)) {
-					//this is kinda normal
-					continue;
-				}
+	unsigned int total_read=0;
 	
 
-				//cerr << my_chr << ":" << my_pos << " " << mate_chr << ":" << mate_pos << endl;
 
-				if (mappings.find(qname)!=mappings.end()) {
-					//cerr << "FOUND " << endl;
-					//lets add it to the clusters
-					pos my = pos(my_chr,my_pos,my_strand);
-					pos mate = mappings[qname];
-					mappings.erase(qname);	
-					
-					pair<pos,pos> key = find_cluster(my,mate);
-					pos left = my;
-					pos right = mate;
-					if (my>mate) {
-						left=mate;
-						right=my;
-					}
-
-
-					
-					clusters[key.first][key.second].lefts.insert(left);					
-					clusters[key.first][key.second].rights.insert(right);					
-
-				} else {
-					//need to add it to mappings
-					//cerr << "ADDING " << qname << endl;
-					mappings[qname]=pos(my_chr,my_pos,my_strand);
-				}
-				
+	vector< string > rows;
+	rows.reserve(READ_SIZE);
+	while (true) {
+		//read in a million
+		char buffer[1024];
+		unsigned int read = 0;
+		for (string row; getline(cin, row, row_delim); ) {
+			read++;
+			rows.push_back(row);
+			if (read==READ_SIZE) {
+				break;
 			}
 		}
-	}
 
+		total_read+=read;
+		cerr << "\r" << "read: " << total_read << "     "; 
+
+		//now process the reads
+		#pragma omp parallel
+		{
+			unsigned int threads = omp_get_num_threads();
+			unsigned int thread_id = omp_get_thread_num();
+			for (unsigned int i=0; i<read; i++) {
+				if (i%threads==thread_id) {
+					//process it
+					vector<string> v_row;
+					istringstream ss(rows[i]);
+					for (string field; getline(ss, field, field_delim); ) {
+						v_row.push_back(field);
+					}
+					process_read(v_row);
+				}
+			}
+		}
+	
+		if (read!=READ_SIZE) {
+			break;
+		}
+
+	}
+	cerr << "CLEAN OUT" << endl;
 	//print out the clusters
 	for (map<pos , map< pos, cluster> >::iterator it=clusters.begin(); it!=clusters.end(); it++) {
 		pos posa = it->first;
 		for (map<pos,cluster>::iterator xit=clusters[it->first].begin(); xit!=clusters[it->first].end(); xit++) {
 			pos posb = xit->first;
 			cluster c = clusters[posa][posb];
+			if (c.lefts.size()==0 || c.rights.size()==0) {
+				continue;
+			}
 			pos left_bound;
 			if (c.left_strand) {
 				left_bound=set_max(c.lefts);
