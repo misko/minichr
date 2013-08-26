f725e8dd764ee25f66cd4a55f02800fa6ab006fb
misko <misko@cs.toronto.edu>
2013-08-26 01:00:18 -0400

    first upload

---
+++ cplex/graph_to_lp.py
@@ -0,0 +1,165 @@
+
+
+import sys
+import gzip
+
+
+if len(sys.argv)!=2:
+	print "%s problemxfile.gz" % sys.argv[0]
+	sys.exit(1)
+
+pbxfname=sys.argv[1]
+
+
+nodes={}
+ins={}
+outs={}
+edges={}
+
+
+'''
+Somatic edges types
+	0 + + >->
+	1 - - <-<
+	2 + - >-<
+	3 - + <->
+'''
+
+edge_lookup=[]
+
+def addxedge(fn,tn,ty,cost,cap):
+	b="px%dx%d" % (fn,tn)
+	if b not in edges:
+		edges[b]=[]
+	eid="%sx%d" % (b,len(edges[b]))
+	edges[b].append((cost,cap))
+	if ty==0:
+		outs[fn].append(eid)
+		ins[tn].append(eid)
+	elif ty==1:
+		ins[fn].append(eid)
+		outs[tn].append(eid)
+	elif ty==2:
+		outs[fn].append(eid)
+		outs[tn].append(eid)
+	elif ty==3:
+		ins[fn].append(eid)
+		ins[tn].append(eid)
+
+def readxpb(fname):
+	h=gzip.open(fname)
+	for line in h:
+		if line[0]=='c':
+			if line[2]=='G':
+				#c Genomic       chr1:712095     chr1:714492     66      4       2397    736     1844
+				#ss << "c Genomic\t" << e.posa.str() << "\t" << e.posb.str()  << "\t" << cost << "\t" << cap << "\t" << e.length() << "\t" << ei.normal << "\t" << ei.tumor << endl;
+				line=line.split()
+				cost=int(line[4])
+				cap=int(line[5])
+				l=int(line[6])
+				normal=int(line[7])
+				tumor=int(line[8])
+				fn=nodes[line[2]]
+				tn=nodes[line[3]]
+				addxedge(fn,tn,0,cost,cap)
+			elif line[2]=='S':
+				#c Somatic       chr1:814892     chrY:11934477   1       23      399     237     33
+				#ss << "c Somatic\t" << e.posa.str() << "\t" << e.posb.str() << "\t" << ei.type << "\t" << cost << "\t" << cap << "\t" << ei.normal << "\t" << ei.tumor << endl;
+				line=line.split()
+				ty=int(line[4])
+				cost=int(line[5])
+				cap=int(line[6])
+				normal=int(line[7])
+				tumor=int(line[8])
+				fn=nodes[line[2]]
+				tn=nodes[line[3]]
+				addxedge(fn,tn,ty,cost,cap)
+			elif line[2]=='N':
+				#its a node
+				#c NODE 19 chr1:808212
+				line=line.split()
+				nid=int(line[2])
+				nodes[line[3]]=nid
+				ins[nid]=[]
+				outs[nid]=[]
+	h.close()
+
+
+
+
+readxpb(pbxfname)
+
+def outputxlpxsolve():
+	objxfunc=[]
+	for e in edges:
+		for x in range(len(edges[e])):
+			eid="%sx%d" % (e,x)
+			if edges[e][x][0]!=0:
+				objxfunc.append("%d %s" % (edges[x][0],eid))
+	print "min: "," + ".join(objxfunc),";"
+	for n in nodes:
+		c=[]
+		for x in ins[nodes[n]]:
+			c.append(" + f%d" % (x))
+		for x in outs[nodes[n]]:
+			c.append(" - f%d" % (x)) 
+		if len(c)!=0:
+			print "".join(c),"= 0",";"
+	for e in edges:
+		for x in range(len(edges[e])):
+			eid="%sx%d" % (e,x)
+			print " %s <= %d" % (eid,edges[eid][x][1]),';'
+	print "int "
+	o=[]
+	for e in edges:
+		for x in range(len(edges[e])):
+			eid="%sx%d" % (e,x)
+			o.append(eid)
+	print ",".join(o),';'
+
+def outputxcplex():
+	#now output the lp file
+	objxfunc=[]
+	for e in edges:
+		for x in range(len(edges[e])):
+			eid="%sx%d" % (e,x)
+			if edges[e][x][0]!=0:
+				objxfunc.append("%d %s" % (edges[e][x][0],eid))
+	print "Minimize\n obj:",
+	ll=5
+	for x in objxfunc:
+		if ll+len(x)+3>=80:
+			ll=0
+			print ""
+			print " +",x,
+			ll+=len(x)+3
+		else:
+			print " +",x,
+			ll+=len(x)+3
+
+	print "Subject To"
+	for n in nodes:
+		c=[]
+		for eid in ins[nodes[n]]:
+			c.append(" + %s" % (eid))
+		for eid in outs[nodes[n]]:
+			c.append(" - %s" % (eid)) 
+		if len(c)!=0:
+			print " n%d: " % nodes[n],"".join(c)," = 0"
+			#print "".join(c),"= 0",";"
+
+	print "Bounds"
+	for e in edges:
+		for x in range(len(edges[e])):
+			eid="%sx%d" % (e,x)
+			print " %s <= %d" % (eid,edges[e][x][1])
+	print "Integers "
+	o=[]
+	for e in edges:
+		for x in range(len(edges[e])):
+			eid="%sx%d" % (e,x)
+			o.append(eid)
+	print " ".join(o)
+	print "End"
+
+outputxcplex()
